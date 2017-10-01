78184ac6840f7257b4972a6152a2a6105fbef689
RetroGameClub <32254569+RetroGameClub@users.noreply.github.com>
2017-10-01 13:58:23 -0700

    Update golang.sh

--- scriptmodules/supplementary/golang.sh
+++ scriptmodules/supplementary/golang.sh
@@ -1,11 +1,12 @@
 # This file is part of The RetroEvolved Project
 #
-# The RetroEvolved Project is the legal property of its developers, whose names are
+# The RetroEvolved Project is a derivative reworking of The RetroPie Project. The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
 #
 # See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/RetroEvolved/RetroEvolved-Setup/master/LICENSE.md
 #
+
 rp_module_id="golang"
 rp_module_desc="Golang binary install"
 rp_module_section=""
