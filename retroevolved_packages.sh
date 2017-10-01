bcc4329664d3bf37fc72e43d1578ce8fb08a4d2b
RetroGameClub <32254569+RetroGameClub@users.noreply.github.com>
2017-10-01 00:53:05 -0700

    Update retroevolved_packages.sh

--- retroevolved_packages.sh
+++ retroevolved_packages.sh
@@ -2,7 +2,7 @@
 
 # This file is part of The RetroEvolved Project
 #
-# The RetroEvolved Project is the legal property of its developers, whose names are
+# The RetroEvolved Project is a derivative reworking of The RetroPie Project. The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
 #
 # See the LICENSE.md file at the top-level directory of this distribution and
@@ -49,7 +49,7 @@ if [[ "$(id -u)" -ne 0 ]]; then
     exit 1
 fi
 
-__backtitle="retroevolved.org.uk - RetroEvolved Setup. Installation folder: $rootdir for user $user"
+__backtitle="retrogame.club - RetroEvolved Setup. Installation folder: $rootdir for user $user"
 
 source "$scriptdir/scriptmodules/system.sh"
 source "$scriptdir/scriptmodules/helpers.sh"
