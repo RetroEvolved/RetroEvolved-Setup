e278cbd53f88015efabfd6a356f85d555013ddfd
RetroGameClub <32254569+RetroGameClub@users.noreply.github.com>
2017-10-01 14:00:37 -0700

    Update scraper.sh

--- scriptmodules/supplementary/scraper.sh
+++ scriptmodules/supplementary/scraper.sh
@@ -2,7 +2,7 @@
 
 # This file is part of The RetroEvolved Project
 #
-# The RetroEvolved Project is the legal property of its developers, whose names are
+# The RetroEvolved Project is a derivative reworking of The RetroPie Project. The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
 #
 # See the LICENSE.md file at the top-level directory of this distribution and
@@ -10,7 +10,7 @@
 #
 
 rp_module_id="scraper"
-rp_module_desc="Scraper for EmulationStation by Steven Selph"
+rp_module_desc="Alternative Scraper for EmulationStation by Steven Selph"
 rp_module_licence="MIT https://raw.githubusercontent.com/sselph/scraper/master/LICENSE"
 rp_module_section="opt"
 
