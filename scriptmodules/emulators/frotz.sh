605f9a567a96fc60e000d439a483ccb49862f85a
RetroGameClub <32254569+RetroGameClub@users.noreply.github.com>
2017-10-01 13:24:41 -0700

    Update frotz.sh

--- scriptmodules/emulators/frotz.sh
+++ scriptmodules/emulators/frotz.sh
@@ -2,13 +2,12 @@
 
 # This file is part of The RetroEvolved Project
 #
-# The RetroEvolved Project is the legal property of its developers, whose names are
+# The RetroEvolved Project is a derivative reworking of The RetroPie Project. The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
 #
 # See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/RetroEvolved/RetroEvolved-Setup/master/LICENSE.md
 #
-
 rp_module_id="frotz"
 rp_module_desc="Z-Machine Interpreter for Infocom games"
 rp_module_help="ROM Extensions: .dat .zip .z1 .z2 .z3 .z4 .z5 .z6 .z7 .z8\n\nCopy your Infocom games to $romdir/zmachine"
