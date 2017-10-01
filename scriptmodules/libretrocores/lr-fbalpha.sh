f7dac0c118e327d5783fdec9aabd0f0461678823
RetroGameClub <32254569+RetroGameClub@users.noreply.github.com>
2017-10-01 13:45:34 -0700

    Update lr-fbalpha.sh

--- scriptmodules/libretrocores/lr-fbalpha.sh
+++ scriptmodules/libretrocores/lr-fbalpha.sh
@@ -2,7 +2,7 @@
 
 # This file is part of The RetroEvolved Project
 #
-# The RetroEvolved Project is the legal property of its developers, whose names are
+# The RetroEvolved Project is a derivative reworking of The RetroPie Project. The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
 #
 # See the LICENSE.md file at the top-level directory of this distribution and
