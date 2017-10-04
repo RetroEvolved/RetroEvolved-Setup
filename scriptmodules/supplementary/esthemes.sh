69d78ee8b711b2911cf23208dfcb2dbb35be5ea8
RetroGameClub <32254569+RetroGameClub@users.noreply.github.com>
2017-10-03 17:44:27 -0700

    Update esthemes.sh

--- scriptmodules/supplementary/esthemes.sh
+++ scriptmodules/supplementary/esthemes.sh
@@ -47,6 +47,7 @@ function gui_esthemes() {
     local themes=(
         'RetroEvolved default'
         'RetroEvolved ComicBook'
+        'RetroEvolved snes-mini'
         'RetroPie carbon'
         'RetroPie carbon-centered'
         'RetroPie carbon-nometa'
